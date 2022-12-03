d2985b63ef547bed2aa2cc23af2c94a8145578f8
Luca Morelli <luca.morelli.lm@gmail.com>
2022-12-03 02:30:13 +0100

    fix: deprecated usage of react-dom.render

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
-import reactDom from "react-dom";
 import App from "./App";
+import { createRoot } from 'react-dom/client';
 
-reactDom.render(<App />, document.getElementById("root"));
+const container = document.getElementById('app');
+const root = createRoot(container!);
+root.render(<App />);
