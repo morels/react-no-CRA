7eae333ed8c739235c50d7ed8281c0d51143b877
Luca Morelli <luca.morelli.lm@gmail.com>
2022-12-03 01:56:42 +0100

    build: add webpack config

---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+const path = require("path");
+const prod = process.env.NODE_ENV === 'production';
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+      mode: prod ? 'production' : 'development',
+      entry: './src/index.tsx',
+      output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: "main.js",
+        publicPath: '/',
+    },
+    target: "web",
+    devServer: {
+        port: process.env.PORT || "3000",
+        static: ["./public"],
+        open: true,
+        hot: true ,
+        liveReload: true,
+        historyApiFallback: {
+            index: '/',
+        },
+    },
+    module:{
+        rules: [
+            {
+                test: /\.(js|ts)x?$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                },
+            },
+        ]
+    },
+    resolve: {
+        extensions: ['.ts', '.tsx', '.js', '.json'],
+    },
+    devtool: prod ? undefined : 'source-map',
+    plugins: [
+        new HtmlWebpackPlugin({
+          template: './src/index.html',
+        }),
+      ],    
+}
