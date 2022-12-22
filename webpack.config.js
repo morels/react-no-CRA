d830fcfe8db22d77bda8427566eea38fb38945f7
Luca Morelli <luca.morelli.lm@gmail.com>
2022-12-22 11:07:55 +0100

    fix: lint

--- webpack.config.js
+++ webpack.config.js
@@ -1,56 +1,57 @@
 const path = require("path");
-const prod = process.env.NODE_ENV === 'production';
 
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const prod = process.env.NODE_ENV === "production";
+
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-      mode: prod ? 'production' : 'development',
-      entry: './src/index.tsx',
-      output: {
-        path: path.resolve(__dirname, "dist"),
-        filename: "main.js",
-        publicPath: '/',
+  mode: prod ? "production" : "development",
+  entry: "./src/index.tsx",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "main.js",
+    publicPath: "/",
+  },
+  target: "web",
+  devServer: {
+    port: process.env.PORT || "3000",
+    static: ["./public"],
+    open: true,
+    hot: true,
+    liveReload: true,
+    historyApiFallback: {
+      index: "/",
     },
-    target: "web",
-    devServer: {
-        port: process.env.PORT || "3000",
-        static: ["./public"],
-        open: true,
-        hot: true ,
-        liveReload: true,
-        historyApiFallback: {
-            index: '/',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|ts)x?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
         },
-    },
-    module:{
-        rules: [
-            {
-                test: /\.(js|ts)x?$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader',
-                },
-            },
-            {
-                test: /\.s[ac]ss$/i,
-                use: [
-                  // Creates `style` nodes from JS strings
-                  "style-loader",
-                  // Translates CSS into CommonJS
-                  "css-loader",
-                  // Compiles Sass to CSS
-                  "sass-loader",
-                ],
-              },
-        ]
-    },
-    resolve: {
-        extensions: ['.ts', '.tsx', '.js', '.json'],
-    },
-    devtool: prod ? undefined : 'source-map',
-    plugins: [
-        new HtmlWebpackPlugin({
-          template: './src/index.html',
-        }),
-      ],    
-}
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          "style-loader",
+          // Translates CSS into CommonJS
+          "css-loader",
+          // Compiles Sass to CSS
+          "sass-loader",
+        ],
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".ts", ".tsx", ".js", ".json"],
+  },
+  devtool: prod ? undefined : "source-map",
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+    }),
+  ],
+};
