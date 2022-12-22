7c850e77c454e062792aa5918d9e1c908e93fff7
Luca Morelli <luca.morelli.lm@gmail.com>
2022-12-22 17:54:41 +0100

    feat: remove support to scss and bootstrap
    - remove scss rules and related webpack loaders
    - remove reactstrap
    - remove bootstrap
    - remove scss import types
    - replace tags in jsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 import React from "react";
-import "./global.scss";
-import { Button } from "reactstrap";
 import { BaseLayout } from "./components/BaseLayout";
 
 function App() {
   return (
-    <BaseLayout title="Welcome to React App thats build without CRA">
-      <Button color="danger">Danger!</Button>
-    </BaseLayout>
+    <>
+      <header />
+      <BaseLayout title="Welcome to React App thats build without CRA">
+        {/* eslint-disable-next-line react/button-has-type */}
+        <button>Danger!</button>
+      </BaseLayout>
+      <footer />
+    </>
   );
 }
 
--- src/components/BaseLayout/base-layout.tsx
+++ src/components/BaseLayout/base-layout.tsx
@@ -1,23 +1,14 @@
 import React, { PropsWithChildren } from "react";
-import { Col, Container, Row } from "reactstrap";
 
 type Props = {
   title: string;
 };
 
 const BaseLayout = ({ title, children }: PropsWithChildren<Props>) => (
-  <Container>
-    <Row>
-      <Col>
-        <h1>{title}</h1>
-      </Col>
-    </Row>
-    <Row>
-      <Col>
-        {children}
-      </Col>
-    </Row>
-  </Container>
+  <main>
+    <h1>{title}</h1>
+    {children}
+  </main>
 );
 
 export { BaseLayout };
